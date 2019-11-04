efa58ac5c5a3c16dd6d6998125f07ccb26f58aee
Ashu Singla <ashusingla884@gmail.com>
2019-11-04 18:39:36 +0530

    Animatable

--- confusion/components/AboutComponent.js
+++ confusion/components/AboutComponent.js
@@ -5,6 +5,7 @@ import { Paragraph } from 'react-native-paper';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { Loading } from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -49,43 +50,38 @@ class About extends Component{
         else if(this.props.leaders.errMess){
             return(
                 <ScrollView>
-                    <RenderHistory />
-                    <Card title='Corporate Leadership'>
+                    <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
                         <Text>{this.props.leaders.errMess}</Text>
                     </Card>
+                    </Animatable.View>
                 </ScrollView>    
             )
         }
         else{
             return(
                 <ScrollView>
-                    <RenderHistory />
-                    <Card title='Corporate Leadership'>
-                        <FlatList data={this.props.leaders.leaders}
-                        keyExtractor={item=>item.id}
-                        renderItem={({item}) => (
-                            <ListItem containerStyle={{borderBottomWidth: 0}}
-                            hideChevron={true}
-                            title={item.name}
-                            subtitle={item.description}
-                            leftAvatar={{source:{uri: baseUrl+item.image }}}/>
-                        )}/>
-                    </Card>
+                    <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+                        <RenderHistory />
+                        <Card title='Corporate Leadership'>
+                            <FlatList data={this.props.leaders.leaders}
+                            keyExtractor={item=>item.id}
+                            renderItem={({item}) => (
+                                <ListItem containerStyle={{borderBottomWidth: 0}}
+                                hideChevron={true}
+                                title={item.name}
+                                subtitle={item.description}
+                                leftAvatar={{source:{uri: baseUrl+item.image }}}/>
+                            )}/>
+                        </Card>
+                    </Animatable.View>
                 </ScrollView>
             );
         }
     }
 }
 
-const styles = StyleSheet.create({
-    image: {
-        borderRadius: 50/2, 
-        height: 50, 
-        width: 50,
-        borderColor:'rgba(0,0,0,0.1)',
-        borderWidth:2,
-        margin:10
-    }
-})
 
 export default connect(mapStateToProps)(About);
