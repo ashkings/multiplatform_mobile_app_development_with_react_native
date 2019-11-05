dde2d00a5e44038113ddfe69c9082e78165ea92f
Ashu Singla <ashusingla884@gmail.com>
2019-11-05 19:36:26 +0530

    Gestures Part 2

--- confusion/components/DishdetailComponent.js
+++ confusion/components/DishdetailComponent.js
@@ -24,6 +24,8 @@ function RenderDish(props) {
 
     const dish = props.dish;
 
+    handleViewRef = ref => this.view = ref;
+
     const recognizeDrag = ({ MoveX, MoveY, dx, dy}) => {
         if(dx < -200)
             return true;
@@ -34,6 +36,10 @@ function RenderDish(props) {
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: (e, gestureState) => {
             return true;
+        },
+        onPanResponderGrant: () => {
+            {this.view.rubberBand(1000)
+                .then(endState => console.log(endState.finished?'finished':'cancelled'));};
         },
         onPanResponderEnd: (e, gestureState) => {
             if(recognizeDrag(gestureState))
@@ -54,7 +60,8 @@ function RenderDish(props) {
         if (dish != null) {
             return(
                 <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
-                 {...panResponder.panHandlers}>
+                ref={this.handleViewRef}
+                {...panResponder.panHandlers}>
                     <Card
                     featuredTitle={dish.name}
                     image={{uri: baseUrl + dish.image}}>
