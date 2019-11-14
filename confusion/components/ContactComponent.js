73e65e33c6f104c2843341d956921d5b7bce6182
Ashu Singla <ashusingla884@gmail.com>
2019-11-14 15:09:40 +0530

    Sending Email

--- confusion/components/ContactComponent.js
+++ confusion/components/ContactComponent.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Card } from 'react-native-elements';
+import { View, Text, ScrollView } from 'react-native';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import email from 'react-native-email'
 
 class Contact extends Component{
 
     static navigationOptions = {
         title: 'Contact',
     }
+
+    sendMail(){
+        const to = ['ashu.singla@hashedin.com'] // string or array of email addresses
+        email(to, {
+            subject: 'Show how to use',
+            body: 'Some body right here'
+        }).catch(console.error)
+    }
+
     render(){
         return(
-            <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>  
-                <Card title='Contact Information'>
-                    <View>
-                        <Text>121, Clear Water Bay Road</Text>
-                        <Text>Clear Water Bay, Kowloon</Text>
-                        <Text>HONG KONG</Text>
-                        <Text>Tel: +852 1234 5678</Text>
-                        <Text>Fax: +852 8765 4321</Text>
-                        <Text>Email:confusion@food.net</Text>
-                    </View>
-                </Card>
-            </Animatable.View>
+            <ScrollView>
+                <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>  
+                    <Card title='Contact Information'>
+                        <View>
+                            <Text>121, Clear Water Bay Road</Text>
+                            <Text>Clear Water Bay, Kowloon</Text>
+                            <Text>HONG KONG</Text>
+                            <Text>Tel: +852 1234 5678</Text>
+                            <Text>Fax: +852 8765 4321</Text>
+                            <Text>Email:confusion@food.net</Text>
+                        </View>
+                        <Button
+                            title="Send Email"
+                            buttonStyle={{backgroundColor: "#512DA8", margin:10}}
+                            icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
+                            onPress={this.sendMail}
+                        />
+                    </Card>
+                </Animatable.View>
+            </ScrollView>
         );
     }
 }
